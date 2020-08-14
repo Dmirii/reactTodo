b56020ffa25369282ed34c3577bd59e49c719919
Dima A <dimaa@dimaa.ru>
2020-08-14 13:31:12 +0300

    add docs

---
+++ docs/precache-manifest.7111e8609376678a741a936ecb66ae2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c37f2f413f41ebfdf88e2555129c59a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e13ac0c559bc86ea2997",
+    "url": "/static/css/main.74827075.chunk.css"
+  },
+  {
+    "revision": "2943a5dd69be043e76cf",
+    "url": "/static/js/2.1e92ccb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e92ccb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ea1f0a0ba1a784fc55",
+    "url": "/static/js/3.415f670f.chunk.js"
+  },
+  {
+    "revision": "e13ac0c559bc86ea2997",
+    "url": "/static/js/main.c2be43e0.chunk.js"
+  },
+  {
+    "revision": "bd564082ff4c6b6cb47b",
+    "url": "/static/js/runtime-main.f6276378.js"
+  }
+]);
