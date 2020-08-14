f97a6c5a1f39b5325c5b23bc861e164b43bc783e
Dima A <dimaa@dimaa.ru>
2020-08-14 13:52:44 +0300

    Updates

---
+++ precache-manifest.f4536c639dc2371b00241f2078ea7181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95027d8aa8a7211da3b760ed4025446c",
+    "url": "/reactCalc/index.html"
+  },
+  {
+    "revision": "0ba3df1588b8843eb597",
+    "url": "/reactCalc/static/css/main.74827075.chunk.css"
+  },
+  {
+    "revision": "2943a5dd69be043e76cf",
+    "url": "/reactCalc/static/js/2.1e92ccb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCalc/static/js/2.1e92ccb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ea1f0a0ba1a784fc55",
+    "url": "/reactCalc/static/js/3.415f670f.chunk.js"
+  },
+  {
+    "revision": "0ba3df1588b8843eb597",
+    "url": "/reactCalc/static/js/main.2c8b4342.chunk.js"
+  },
+  {
+    "revision": "2423c43058354de1a3db",
+    "url": "/reactCalc/static/js/runtime-main.8897a28c.js"
+  }
+]);
